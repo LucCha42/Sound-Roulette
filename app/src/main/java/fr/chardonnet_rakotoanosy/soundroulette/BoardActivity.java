7f11c12887ece90898ef2429c9b9d04f2977ed86
LucCha42 <55276692+LucCha42@users.noreply.github.com>
2020-10-08 21:29:17 +0200

    Listeners de l'activitée principale.

--- app/src/main/java/fr/chardonnet_rakotoanosy/soundroulette/BoardActivity.java
+++ app/src/main/java/fr/chardonnet_rakotoanosy/soundroulette/BoardActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Board extends AppCompatActivity {
+public class BoardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/fr/chardonnet_rakotoanosy/soundroulette/MainActivity.java
+++ app/src/main/java/fr/chardonnet_rakotoanosy/soundroulette/MainActivity.java
@@ -2,13 +2,41 @@ package fr.chardonnet_rakotoanosy.soundroulette;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button gotoBoardButton;
+    private Button randomButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // getting buttons
+        gotoBoardButton = findViewById(R.id.GotoBoardButton); // auto cast
+        randomButton = findViewById(R.id.RandomButton);
+
+        // go to board button listener
+        gotoBoardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), BoardActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // random button listener
+        randomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO random function
+            }
+        });
     }
+
 }
