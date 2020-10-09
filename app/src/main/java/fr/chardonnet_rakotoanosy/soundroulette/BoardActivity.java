c49c0c22bc545b87e0d0345fedfb52cf03c77db5
LucCha42 <55276692+LucCha42@users.noreply.github.com>
2020-10-09 09:41:02 +0200

    Processus de récupération de fichier.

--- app/src/main/java/fr/chardonnet_rakotoanosy/soundroulette/BoardActivity.java
+++ app/src/main/java/fr/chardonnet_rakotoanosy/soundroulette/BoardActivity.java
@@ -1,14 +1,45 @@
 package fr.chardonnet_rakotoanosy.soundroulette;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
 
 public class BoardActivity extends AppCompatActivity {
+
+    private Button addButton;
+    public final static int REQUEST_LOAD_SOUND = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
+
+        addButton = findViewById(R.id.AddButton);
+
+        // add a sound file listener
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("audio/*");
+                startActivityForResult(intent, REQUEST_LOAD_SOUND);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_LOAD_SOUND && requestCode == RESULT_OK) {
+            File soundFile = new File(data.getData().toString());
+            //TODO add new file to local storage
+        }
     }
 }
