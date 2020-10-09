2116c9082b1435807ec71153017f29b611063449
rakotoew <55093676+rakotoew@users.noreply.github.com>
2020-10-09 08:47:18 +0200

    Ajout fonctionnalité bouton navigation

--- app/src/main/java/fr/chardonnet_rakotoanosy/soundroulette/BoardActivity.java
+++ app/src/main/java/fr/chardonnet_rakotoanosy/soundroulette/BoardActivity.java
@@ -2,13 +2,38 @@ package fr.chardonnet_rakotoanosy.soundroulette;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BoardActivity extends AppCompatActivity {
+    private Button goToMainButton;
+    private Button addSongButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
+
+        //getting buttons
+        goToMainButton = findViewById(R.id.GotoMainButton);
+        addSongButton = findViewById(R.id.AddButton);
+        //Listener GotoRandomButton
+        goToMainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // random button listener
+        addSongButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO add function
+            }
+        });
     }
 }
